89765b194d841eedd5c21a6802f386a5924eadef
Gabriel Barbosa <gabriel.indra.br@gmail.com>
2024-09-09 15:05:13 -0300

    Ajustes na classe ContaTerminal para melhor funcionamento Ajustes no arquivo gitignore

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,12 +3,13 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-           
+        double saldo;
+
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("");       
+
+        System.out.println("");
         System.out.println("Digite seu nome: ");
         String nomeCliente = scanner.next();
 
@@ -25,19 +26,25 @@ public class ContaTerminal {
         int numeroConta = scanner.nextInt();
 
         System.out.println("");
-        System.out.println("Digite seu saldo inicial :) ");
-        Double saldo = scanner.nextDouble();
-
-        // String str = String.format("%.02f", saldo);
-        // saldo = Double.valueOf(str);
-
-        DecimalFormat formato = new DecimalFormat("#.##");
-        saldo = Double.valueOf(formato.format(saldo));
+        System.out.println("Digite o saldo no formato 4.500,30 (com ponto e vírgula como separadores):");
+        String entrada = scanner.next();
+        String saldoFormatado;
+        String valorSemPonto = entrada.replace(".", "").replace(",", ".");
+        saldo = Double.parseDouble(valorSemPonto);
+        DecimalFormat formatador = new DecimalFormat("#,##0.00");
+        saldoFormatado = String.format(formatador.format(saldo));
 
         System.out.println("");
-        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + 
-        ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + 
-        ", sua conta: " + numeroConta + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+        String fichaTecnica = """
+                	************************************************************
+                    Olá %s %s.
+                	Obrigado por criar uma conta em nosso banco. 
+                	O número de sua agência é: %s.
+                	Sua conta é: %d.
+                	O saldo de R$%s já está disponível para saque.
+                	************************************************************
+                \t""".formatted(nomeCliente, sobrenomeCliente, agencia, numeroConta, saldoFormatado);
+        System.out.println(fichaTecnica);
 
         scanner.close();
     }
